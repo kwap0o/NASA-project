84a6d9be49953f92eb858896ba0403cb155e16b8
kwap0o <kwapiszr@gmail.com>
2024-10-19 10:07:43 +0200

    Create basic App view

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <div class="nav">
+      <router-link to="home" class="nav-link">Spacer</router-link>
+      <router-link to="about" class="nav-link">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
